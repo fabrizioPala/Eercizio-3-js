ed9acdae4f8427b25cf53fc17df707bf2d7c7239
fabrizioPala <148344967+fabrizioPala@users.noreply.github.com>
2023-11-24 15:30:53 +0100

    Update esercizio6 riepilogo metodi.js

--- esercizio6 riepilogo metodi.js
+++ esercizio6 riepilogo metodi.js
@@ -1,36 +1,27 @@
-/*Utilizza forEach per stampare i nomi degli studenti.
-Utilizza find per trovare uno studente con un voto superiore a 90.
-Utilizza reduce per calcolare la media dei voti degli studenti.
-Utilizza map per creare un nuovo array contenente i nomi degli studenti in maiuscolo.
-Utilizza filter per trovare gli studenti con voti superiori a 85.
 const studenti = [
   { nome: "Alice", voto: 95 },
   { nome: "Bob", voto: 88 },
   { nome: "Carol", voto: 76 },
   { nome: "David", voto: 92 },
   { nome: "Eve", voto: 84 },
-];*/
-const studenti = [
-    { nome: "Alice", voto: 95 },
-    { nome: "Bob", voto: 88 },
-    { nome: "Carol", voto: 76 },
-    { nome: "David", voto: 92 },
-    { nome: "Eve", voto: 84 },
-  ]
-  //FOR EACH
- studenti.forEach(element=> console.log(element.nome));
- //FIND
- let studenteVotoAlto=studenti.find(student=>student.voto>90) 
- console.log(studenteVotoAlto)
- //REDUCE
- function average(array){ 
-    let sum=array.reduce((acc,person)=> acc+person.voto,0)
-    let average= sum/array.length
-    return average ;
-}
-//MAP
-let mapStudents=studenti.map(student=> student.nome.toUpperCase())
-console.log(mapStudents)
-//Filter
-let filteredStudents=studenti.filter(alumn=>alumn.voto>85)
-console.log(filteredStudents)
+];
+
+// forEach
+console.log("Nomi degli studenti:");
+studenti.forEach(student => console.log(student.nome));
+
+// find
+const studenteVotoAlto = studenti.find(student => student.voto > 90);
+console.log("Studente con voto superiore a 90:", studenteVotoAlto);
+
+// reduce
+const averageVoto = studenti.reduce((acc, student) => acc + student.voto, 0) / studenti.length;
+console.log("Media dei voti degli studenti:", averageVoto);
+
+// map
+const mapStudents = studenti.map(student => student.nome.toUpperCase());
+console.log("Nomi degli studenti in maiuscolo:", mapStudents);
+
+// filter
+const filteredStudents = studenti.filter(student => student.voto > 85);
+console.log("Studenti con voti superiori a 85:", filteredStudents);
